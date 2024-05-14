70a3216ef01bc3f3061e2f755a2aaf5dfa6928e8
Mihaly24 <fairuzk20@gmail.com>
2024-05-14 18:47:06 +0700

    Menambahkan class mahasiswa dan isinya

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+    void cetak() {
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    };
+};
+
 int main()
 {
     
