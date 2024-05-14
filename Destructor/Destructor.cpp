d6c290b00ec0b7c0265efa0bb5845aaaafbd2b5e
Mihaly24 <fairuzk20@gmail.com>
2024-05-14 19:08:40 +0700

    Menyelesaikan projek destructor

--- Destructor/Destructor.cpp
+++ Destructor/Destructor.cpp
@@ -12,7 +12,38 @@ public:
     void isiData();
 };
 
+angka::angka(int i) {
+    panjang = 1;
+    arr = new int(1);
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat array sudah dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << 1 << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << 1 << " = ";
+        cin >> arr[1];
+    }
+    cout << endl;
+}
+
 int main()
 {
-    
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
 }
