fefcd24e0dbfb480d27701ad2183eb1fb43ae0e6
Mihaly24 <fairuzk20@gmail.com>
2024-05-14 18:56:23 +0700

    Menambahkan class angka serta isinya

--- Destructor/Destructor.cpp
+++ Destructor/Destructor.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+using namespace std;
+
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
+};
 
 int main()
 {
