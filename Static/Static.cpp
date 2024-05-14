54baa0c14fed9be18e815741607d8c0f90001165
Mihaly24 <fairuzk20@gmail.com>
2024-05-14 17:43:22 +0700

    Menyelesaikan class mahasiswa

--- Static/Static.cpp
+++ Static/Static.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 class mahasiswa {
 public:
-}
+    static int nim;
+    int id;
+    string nama;
+
+    void setId();
+    void printAll();
+    mahasiswa(string pnama) {
+        nama = pnama;
+        setId();
+    }
+};
 
 int main()
 {
